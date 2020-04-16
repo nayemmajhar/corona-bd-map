91ec12e4ea7fa0cd8e3c4e8e83c8c22b437eb462
nayemmajhar <nayemmajhar@gmail.com>
2020-04-16 13:42:16 +0200

    site url settings based on env

--- apps/ctweb/src/helpers/ctKielApi.js
+++ apps/ctweb/src/helpers/ctKielApi.js
@@ -4,9 +4,8 @@
 // }
 
 const ctKielApi ={
-    URL: 'http://68.183.64.86:8080/v1',
-    siteUrl: 'http://68.183.64.86:3000',
-    graphHopperKey: "f60ff2c2-e959-4826-a351-dfea1ba2b23b"
+    URL: (process.env.NODE_ENV === 'development')? 'http://127.0.0.1:8080/v1':'http://68.183.64.86:8080/v1',
+    siteUrl: (process.env.NODE_ENV === 'development')? 'http://localhost:3000':'http://68.183.64.86:3000'
 }
 
 export default ctKielApi;
