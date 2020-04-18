9bdc6e1a2c13bfdf31b9f0405eb8257b7d9fd5c4
nayemmajhar <nayemmajhar@gmail.com>
2020-04-19 01:58:15 +0200

    ctweb beta 1.0.1

--- apps/ctweb/src/App.js
+++ apps/ctweb/src/App.js
@@ -5,9 +5,6 @@ import Header from './components/Header/index';
 import Footer from './components/Footer';
 import Home from './pages/Home/index'
 import TourMap from './pages/TourMap/index'
-import Tours from './pages/Tours/index'
-import Places from './pages/Places/index'
-import PlaceMap from './pages/PlaceMap/index'
 
 
 
--- apps/ctweb/src/helpers/ctKielApi.js
+++ apps/ctweb/src/helpers/ctKielApi.js
@@ -4,8 +4,8 @@
 // }
 
 const ctKielApi ={
-    URL: (process.env.NODE_ENV === 'development')? 'http://127.0.0.1:8080/v1':'http://68.183.64.86:8080/v1',
-    siteUrl: (process.env.NODE_ENV === 'development')? 'http://localhost:3000':'http://68.183.64.86:3000'
+    URL: (process.env.NODE_ENV === 'development')? 'http://127.0.0.1:8080/v1':'http://data.coronameterbd.com/v1',
+    siteUrl: (process.env.NODE_ENV === 'development')? 'http://localhost:3000':'http://coronameterbd.com'
 }
 
 export default ctKielApi;
